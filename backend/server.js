dc02266a641f424dcb10989930c0eb491fac58d4
369webforall <369webforall@gmail.com>
2023-07-25 16:57:18 +0545

    backend completed

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/userModel');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const router = require('./routes/user-routs');
+
+//get, post, put, delete
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('server is running in port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use('/', router);
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div className="bg-orange-600 py-4">App</div>;
+};
+
+export default App;
